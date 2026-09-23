5077f9e24ca6f3b3e8d69df332d0db8e0a99360f
aashihere11 <meenaaashi2007@gmail.com>
2026-09-23 21:58:45 +0530

    style: minor change in  UI

--- frontend/src/Components/dashboard/OrderProduct.jsx
+++ frontend/src/Components/dashboard/OrderProduct.jsx
@@ -16,7 +16,7 @@ function OrderProduct() {
             <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5" >
 
                 {/* RECENT ORDERS */}
-                <div className='bg-[#fff] rounded-md p-4 border-2 border-[#e5e7eb] shadow-md '>
+                <div className='bg-[#fff] rounded-md p-4 border-2 border-[#e5e7eb] shadow-md overflow-y-auto scrollbar-thin scrollbar-thumb-[#a8e0b5] max-h-[500px] '>
                     <div className='text-sm md:text-xl font-medium text-[#111] mb-4 flex items-center justify-between'>
                         <span>Recent orders</span>
                         <span className='text-sm md:text-base text-[2e8a48] border-1 border-[#a8e0b5] shadow-sm rounded-3xl px-2'>View all</span>
@@ -24,17 +24,17 @@ function OrderProduct() {
                     {orders.map((o) => {
 
                         return (
-                            <div key={o.id} className='flex items-center gap-1 md:gap-5  rounded-md border-b border-gray-200'>
-                                <div className='w-15 h-15 md:w-30 md:h-30 rounded-sm flex items-center justify-center text-base shrink-0 overflow-hidden shrink'>{o.emoji}</div>
+                            <div key={o.id} className='flex items-center gap-1 md:gap-5  rounded-md border-b border-gray-200 shrink'>
+                                <div className='w-15 h-15 md:w-30 md:h-30 rounded-sm flex items-center justify-center text-base  overflow-hidden shrink'>{o.emoji}</div>
                                 <div >
-                                    <div className='text-sm md:text-xl font-medium text-[#111]'>{o.name} · {o.qty}</div>
-                                    <div className='text-sm md:text-base text-[#6b7280] mt-2' >{o.buyer} · {o.time}</div>
+                                    <p className='text-xs md:text-xl font-medium text-[#111] m-0'>{o.name} · {o.qty}</p>
+                                    <p className='text-xs md:text-base text-[#6b7280] mt-2 m-0' >{o.buyer} · {o.time}</p>
                                 </div>
                                 <div className='ml-auto text-center '>
-                                    <div className="text-sm md:text-xl font-medium text-[#1a5c2e]" >{o.price}</div>
-                                    <div className='text-sm md:text-base font-medium  rounded-xl bg-gray-200 px-2 mt-2 ' >
+                                    <p className="text-xs md:text-xl font-medium text-[#1a5c2e]" >{o.price}</p>
+                                    <p className='text-[10px] md:text-base  rounded-xl bg-gray-200 px-2 mt-2 ' >
                                         {o.status}
-                                    </div>
+                                    </p>
                                 </div>
                             </div>
                         );
@@ -42,29 +42,28 @@ function OrderProduct() {
                 </div>
 
                 {/* MY PRODUCTS */}
-                <div className='bg-[#fff] rounded-md p-4 border-2 border-[#e5e7eb] shadow-md '>
+                <div className='bg-[#fff] rounded-md p-4 border-2 border-[#e5e7eb] shadow-md
+                 overflow-y-auto scrollbar-thin scrollbar-thumb-[#a8e0b5] max-h-[500px]'
+                  style={{ background: "linear-gradient(135deg, #e8f4fd, #fff)" }}>
                     <div className='text-sm md:text-xl font-medium text-[#111] mb-4 flex items-center justify-between'>
                         <span>My products</span>
-                        <span className='text-sm md:text-base text-[2e8a48] border-1 border-[#a8e0b5] shadow-sm rounded-3xl px-2' >Manage</span>
+                        <span className='text-xs md:text-base text-[2e8a48] border-1 border-[#a8e0b5] shadow-sm rounded-3xl px-2' >Manage</span>
                     </div>
                     {products.map((p) => (
                         <div key={p.id} className='flex items-center gap-1 md:gap-5  rounded-md border-b border-gray-200' >
                             <div className='w-15 h-15 md:w-30 md:h-30 rounded-sm flex items-center justify-center text-base shrink-0'>{p.emoji}</div>
                             <div >
-                                <div className='text-sm md:text-xl font-medium text-[#111]'>{p.name}</div>
-                                <div className='text-sm md:text-xl text-[#6b7280]  mt-1'>{p.price} </div>
+                                <div className='text-xs md:text-xl font-medium text-[#111]'>{p.name}</div>
+                                <div className='text-xs md:text-xl text-[#6b7280]  mt-1'>{p.price} </div>
                             </div>
                             <div className='ml-auto text-center'>
-                                <p className={getStockColor(p.stock)}>{p.stock}</p>
-                                {p.stock == 0 ? "Out of stock" : `${p.stock} ${p.unit} left`}
+                                <p className={`text-xs md:text-base font-medium m-0 ${getStockColor(p.stock)}`}>{p.stock}</p>
+                                <p className='text-xs md:text-base font-medium m-0'>{p.stock == 0 ? "Out of stock" : `${p.stock} ${p.unit} left`}</p>
                             </div>
                         </div>
 
 
                     ))}
-                    <button className='flex items-center justify-center text-sm  py-3 font-medium text-[#2e8a48] bg-[#f0faf2]
-                  border-1 border-[#a8e0b5] shadow-md pointer rounded-md font-serif w-full' style={{ borderRadius: "10px" }}
-                        onClick={() => navigate("/myproducts")}>➕ Add new product</button>
                 </div>
             </div>
 
--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -6,23 +6,28 @@ import TabSwitcher from '../Components/dashboard/TabSwitcher';
 function DashboardPage() {
 
   return (<>
+ <nav className="flex items-center justify-between bg-white border-b border-gray-100 sticky top-0 z-10 h-24 p-3">
 
-    <div className='bg-[#f4f7f4] min-h-screen p-6'>
+        <a href="" className="w-60">
+          <img src="media/logo.png" alt="logo" />
+        </a>
+        
+      </nav>
+    <div className='bg-[#f4f7f4] min-h-screen p-3'>
       {/* TOP BAR */}
-      <div>
-        <TemporaryDrawer open={open} onClose={() => setOpen(false)} />
-      </div>
-      <div className='flex itms-center justify-between  ' >
+      
+      <div className='flex itms-center justify-between p-2  ' >
         <div>
-          <h1 className='md:text-base text-sm
+          <p className=' text-sm md:text-3xl
            font-serif font-medium text-[#1a5c2e] m-0' >
             Good morning, Ramesh 🌿
-          </h1>
-          <p className=" text-sm md:text-base text-[#6b7280]" >
+          </p>
+          <p className=" text-xs md:text-base text-[#6b7280]" >
             Here's what's happening on your farm today
           </p>
         </div>
-        <div className='text-sm text-[#6b7280] bg-[#fff] border-1 border-[#e0e0e0] rounded-3xl text-center w-40 h-10 px-2 mr-3 mt-2'>
+        <div className='text-[10px] md:text-sm text-[#6b7280] bg-[#fff] border-1 border-[#e0e0e0] rounded-3xl text-center
+         h-10 px-2 flex items-center'>
           📅 Saturday, 6 June 2026
         </div>
       </div>
@@ -30,13 +35,14 @@ function DashboardPage() {
 
       {/* SECTION 1 — STATS */}
       <p className='text-sm md:text-base uppercase text-[#6b7280] font-medium mb-4'>Today's overview</p>
-      <div className='grid md:grid-cols-4 grid-cols-2 gap-4 ' >
+      <div className='grid md:grid-cols-4 grid-cols-2 md:gap-5 ' >
         {stats.map((s, i) => (
-          <div key={i} className='bg-[#fff] border-1 border-[#e5e7eb] shadow-md rounded-md  md:p-3 p-2 h-40 md:h-auto '>
-            <h5 className="md:text-xl text-base mb-2  m-0" >{s.icon}</h5>
-            <p className='md:text-base text-sm  text-[#6b7280] mb-2 uppercase tracking-wide ' >{s.label}</p>
-            <p className="md:text-lg text-sm font-medium text-[1a5c2e] ">{s.val}</p>
-            <p className={`trend-${s.trend}`}>{s.sub}</p>
+          <div key={i} className='bg-[#fff] border-1 border-[#e5e7eb] shadow-md rounded-md  md:p-3 p-3 md:w-full md:h-full '
+           style={{ background: "linear-gradient(135deg, #e8f4fd, #fff)" }}>
+            <h5 className="md:text-xl text-sm mb-2  m-0" >{s.icon}</h5>
+            <p className='md:text-base text-xs  text-[#6b7280] mb-2 uppercase tracking-wide ' >{s.label}</p>
+            <p className="md:text-lg text-xs font-medium text-[1a5c2e] ">{s.val}</p>
+            <p className={`trend-${s.trend} md:text-base text-xs`}>{s.sub}</p>
           </div>
         ))}
       </div>
